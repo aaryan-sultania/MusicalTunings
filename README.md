eeccc4d08686cfd9e397c9cae28bdd3e49d1dcde
Aaryan Sultania <98294145+aaryan-sultania@users.noreply.github.com>
2025-05-27 16:26:13 +0000

    Restructure everything

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MusicalTunings
-A List of Python Scripts for a paper.
+A list of python scripts for a paper in the Howard County Math Journal.
+## Tuning Calculations
+A folder of scripts used to quickly calculate ratios and note names for different tuning systems
+## Audio Sample Generation
+A folder of scripts used to generate audio samples in the paper.
--- audio_sample_generation/harmonic_series.py
+++ audio_sample_generation/harmonic_series.py
@@ -5,4 +5,8 @@ audioContext = AudioContex(25, 44100, 0.5)
 fundamental = 220
 for i in range(20):
     audioContext.add_wave(i, audioContext.total_duration-i, 1/(2*i+1), fundamental*(2*i+1), 0)
-audioContext.get_wav("harmonic_series.wav")
+audioContext.get_wav("audio_samples/harmonic_series.wav")
+
+squareContext = AudioContex(15, 44100, 0.5)
+squareContext.add_wave(1, 10, 1, fundamental, 1)
+squareContext.get_wav("audio_samples/square.wav")
